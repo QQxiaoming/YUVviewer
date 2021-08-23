04b4e97494b5f42bf5b5bbe35d47504c543f41c2
xiaoming <2014500726@smail.xtu.edu.cn>
2021-08-23 18:42:43 +0800

    add stddll for inno setup

--- build_setup.bat
+++ build_setup.bat
@@ -42,6 +42,7 @@ xcopy /y .\test\* .\InnoSetup\build\test\
 windeployqt --dir .\InnoSetup\build .\InnoSetup\build\YUVviewer.exe
 xcopy /y "%OPENCV_DIR%\libopencv_imgproc420.dll" ".\InnoSetup\build\"
 xcopy /y "%OPENCV_DIR%\libopencv_core420.dll" ".\InnoSetup\build\"
+xcopy /y "%QT_TOOLS_DIR%\..\lib\*.dll" ".\InnoSetup\build\"
 :: 打包
 echo "wait inno build setup..."
 iscc /q ".\InnoSetup\build_temp_setup.iss"
