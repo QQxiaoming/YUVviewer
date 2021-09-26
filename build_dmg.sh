ce8edd402e712d699005d276f00a2ec5fe88985b
xiaoming <2014500726@smail.xtu.edu.cn>
2021-09-26 18:52:04 +0800

    build: update macos build
    Signed-off-by: xiaoming <2014500726@smail.xtu.edu.cn>

--- build_dmg.sh
+++ build_dmg.sh
@@ -27,6 +27,14 @@ make
 cp -R ./test ./build_release/out/YUVviewer.app/contents/resources/test
 cd ./build_release/out
 # 打包
+macdeployqt YUVviewer.app
+cp /usr/local/lib/libopencv_imgproc.dylib ./YUVviewer.app/Contents/Frameworks/libopencv_imgproc.dylib
+cp /usr/local/lib/libopencv_core.dylib ./YUVviewer.app/Contents/Frameworks/libopencv_core.dylib
+cd ./YUVviewer.app/Contents/MacOS
+otool -L YUVviewer
+install_name_tool -change /usr/local/lib/libopencv_imgproc.4.0.0.dylib @executable_path/../Frameworks/libopencv_imgproc.dylib YUVviewer
+install_name_tool -change /usr/local/lib/libopencv_core.4.0.0.dylib @executable_path/../Frameworks/libopencv_core.dylib YUVviewer
+otool -L YUVviewer
 macdeployqt YUVviewer.app -dmg -verbose=2
 echo build success!
 ###############################################################################
