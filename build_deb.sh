398fdcb281ab5853a164bad88e43212e609923fc
xiaoming <2014500726@smail.xtu.edu.cn>
2022-07-28 13:24:49 +0800

    build:change tools to linuxdeploy
    Signed-off-by: xiaoming <2014500726@smail.xtu.edu.cn>

--- build_deb.sh
+++ build_deb.sh
@@ -30,32 +30,20 @@ rm -rf ./dpkg/YUVviewer_Linux_"$YUVVIEWER_VERSION"_x86_64
 rm -f ./dpkg/YUVviewer_Linux_"$YUVVIEWER_VERSION"_x86_64.deb
 # 构建打包目录
 cp -r ./dpkg/YUVviewer ./dpkg/YUVviewer_Linux_"$YUVVIEWER_VERSION"_x86_64
+# 使用linuxdeployqt拷贝依赖so库到打包目录
+export QMAKE=$QT_DIR/bin/qmake
+./tools/linuxdeploy-x86_64.AppImage --executable=./build_release/out/YUVviewer --appdir=./dpkg/YUVviewer_Linux_"$YUVVIEWER_VERSION"_x86_64/opt --plugin=qt
+rm -rf ./dpkg/YUVviewer_Linux_"$YUVVIEWER_VERSION"_x86_64/opt/apprun-hooks
+mv ./dpkg/YUVviewer_Linux_"$YUVVIEWER_VERSION"_x86_64/opt/usr ./dpkg/YUVviewer_Linux_"$YUVVIEWER_VERSION"_x86_64/opt/YUVviewer
+mv ./dpkg/YUVviewer_Linux_"$YUVVIEWER_VERSION"_x86_64/opt/YUVviewer/bin/YUVviewer ./dpkg/YUVviewer_Linux_"$YUVVIEWER_VERSION"_x86_64/opt/YUVviewer/YUVviewer
+mv ./dpkg/YUVviewer_Linux_"$YUVVIEWER_VERSION"_x86_64/opt/YUVviewer/bin/qt.conf ./dpkg/YUVviewer_Linux_"$YUVVIEWER_VERSION"_x86_64/opt/YUVviewer/qt.conf
+rm -rf ./dpkg/YUVviewer_Linux_"$YUVVIEWER_VERSION"_x86_64/opt/YUVviewer/bin
+sed -i "s/Prefix = ..\//Prefix = .\//g" ./dpkg/YUVviewer_Linux_"$YUVVIEWER_VERSION"_x86_64/opt/YUVviewer/qt.conf
+chrpath -r "\$ORIGIN/./lib" ./dpkg/YUVviewer_Linux_"$YUVVIEWER_VERSION"_x86_64/opt/YUVviewer/YUVviewer
+rm -rf ./dpkg/YUVviewer_Linux_"$YUVVIEWER_VERSION"_x86_64/opt/YUVviewer/share
+cp ./img/ico.png ./dpkg/YUVviewer_Linux_"$YUVVIEWER_VERSION"_x86_64/opt/YUVviewer/YUVviewer.png
 mkdir -p ./dpkg/YUVviewer_Linux_"$YUVVIEWER_VERSION"_x86_64/opt/YUVviewer
 cp -r ./test ./dpkg/YUVviewer_Linux_"$YUVVIEWER_VERSION"_x86_64/opt/YUVviewer/test
-cp ./build_release/out/YUVviewer ./dpkg/YUVviewer_Linux_"$YUVVIEWER_VERSION"_x86_64/opt/YUVviewer/YUVviewer
-# 使用linuxdeployqt拷贝依赖so库到打包目录
-./tools/linuxdeployqt ./dpkg/YUVviewer_Linux_"$YUVVIEWER_VERSION"_x86_64/opt/YUVviewer/YUVviewer -appimage
-# 由于linuxdeployqt目前（截止2022.07.18）还不支持Qt6的plugins依赖分析，这里我们手动拷贝
-mkdir ./dpkg/YUVviewer_Linux_"$YUVVIEWER_VERSION"_x86_64/opt/YUVviewer/plugins
-mkdir ./dpkg/YUVviewer_Linux_"$YUVVIEWER_VERSION"_x86_64/opt/YUVviewer/plugins/iconengines
-cp $QT_PLUGIN_PATH/iconengines/libqsvgicon.so ./dpkg/YUVviewer_Linux_"$YUVVIEWER_VERSION"_x86_64/opt/YUVviewer/plugins/iconengines/libqsvgicon.so
-mkdir ./dpkg/YUVviewer_Linux_"$YUVVIEWER_VERSION"_x86_64/opt/YUVviewer/plugins/imageformats
-cp $QT_PLUGIN_PATH/imageformats/libqgif.so ./dpkg/YUVviewer_Linux_"$YUVVIEWER_VERSION"_x86_64/opt/YUVviewer/plugins/imageformats/libqgif.so
-cp $QT_PLUGIN_PATH/imageformats/libqico.so ./dpkg/YUVviewer_Linux_"$YUVVIEWER_VERSION"_x86_64/opt/YUVviewer/plugins/imageformats/libqico.so
-cp $QT_PLUGIN_PATH/imageformats/libqjpeg.so ./dpkg/YUVviewer_Linux_"$YUVVIEWER_VERSION"_x86_64/opt/YUVviewer/plugins/imageformats/libqjpeg.so
-cp $QT_PLUGIN_PATH/imageformats/libqsvg.so ./dpkg/YUVviewer_Linux_"$YUVVIEWER_VERSION"_x86_64/opt/YUVviewer/plugins/imageformats/libqsvg.so
-mkdir ./dpkg/YUVviewer_Linux_"$YUVVIEWER_VERSION"_x86_64/opt/YUVviewer/plugins/platforminputcontexts
-cp $QT_PLUGIN_PATH/platforminputcontexts/libcomposeplatforminputcontextplugin.so ./dpkg/YUVviewer_Linux_"$YUVVIEWER_VERSION"_x86_64/opt/YUVviewer/plugins/platforminputcontexts/libcomposeplatforminputcontextplugin.so
-cp $QT_PLUGIN_PATH/platforminputcontexts/libibusplatforminputcontextplugin.so ./dpkg/YUVviewer_Linux_"$YUVVIEWER_VERSION"_x86_64/opt/YUVviewer/plugins/platforminputcontexts/libibusplatforminputcontextplugin.so
-mkdir ./dpkg/YUVviewer_Linux_"$YUVVIEWER_VERSION"_x86_64/opt/YUVviewer/plugins/platforms
-cp $QT_PLUGIN_PATH/platforms/libqxcb.so ./dpkg/YUVviewer_Linux_"$YUVVIEWER_VERSION"_x86_64/opt/YUVviewer/plugins/platforms/libqxcb.so
-mkdir ./dpkg/YUVviewer_Linux_"$YUVVIEWER_VERSION"_x86_64/opt/YUVviewer/plugins/xcbglintegrations
-cp $QT_PLUGIN_PATH/xcbglintegrations/libqxcb-egl-integration.so ./dpkg/YUVviewer_Linux_"$YUVVIEWER_VERSION"_x86_64/opt/YUVviewer/plugins/xcbglintegrations/libqxcb-egl-integration.so
-cp $QT_PLUGIN_PATH/xcbglintegrations/libqxcb-glx-integration.so ./dpkg/YUVviewer_Linux_"$YUVVIEWER_VERSION"_x86_64/opt/YUVviewer/plugins/xcbglintegrations/libqxcb-glx-integration.so
-cp $QT_DIR/lib/libQt6XcbQpa.so.6 ./dpkg/YUVviewer_Linux_"$YUVVIEWER_VERSION"_x86_64/opt/YUVviewer/lib/libQt6XcbQpa.so.6
-cp $QT_DIR/lib/libQt6OpenGL.so.6 ./dpkg/YUVviewer_Linux_"$YUVVIEWER_VERSION"_x86_64/opt/YUVviewer/lib/libQt6OpenGL.so.6
-rm -rf ./dpkg/YUVviewer_Linux_"$YUVVIEWER_VERSION"_x86_64/opt/YUVviewer/doc ./dpkg/YUVviewer_Linux_"$YUVVIEWER_VERSION"_x86_64/opt/YUVviewer/default.png ./dpkg/YUVviewer_Linux_"$YUVVIEWER_VERSION"_x86_64/opt/YUVviewer/AppRun ./dpkg/YUVviewer_Linux_"$YUVVIEWER_VERSION"_x86_64/opt/YUVviewer/default.desktop
-cp ./img/ico.png ./dpkg/YUVviewer_Linux_"$YUVVIEWER_VERSION"_x86_64/opt/YUVviewer/YUVviewer.png
 # 配置打包信息
 sed -i "s/#VERSION#/$YUVVIEWER_MAJARVERSION.$YUVVIEWER_SUBVERSION$YUVVIEWER_REVISION/g" ./dpkg/YUVviewer_Linux_"$YUVVIEWER_VERSION"_x86_64/DEBIAN/control
 SIZE=$(du -sh -B 1024 ./dpkg/YUVviewer_Linux_"$YUVVIEWER_VERSION"_x86_64 | sed "s/.\///g")
